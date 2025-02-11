b91c1f4affa491f7f66ea99f99a5ae15f7733e9f
Chris Nanninga <chris.nanninga@bigcommerce.com>
2025-02-11 15:21:00 -0600

    Add component shell for subcategory list

--- core/app/[locale]/(default)/(faceted)/category/[slug]/page.tsx
+++ core/app/[locale]/(default)/(faceted)/category/[slug]/page.tsx
@@ -23,6 +23,7 @@ import { CategoryViewed } from './_components/category-viewed';
 import { getCategoryPageData } from './page-data';
 
 import { getSubcategories } from '~/components/subcategory-list/component-data';
+import { SubcategoryList } from '~/components/subcategory-list';
 
 const cacheCategoryFacetedSearch = cache((categoryId: string) => {
   return { category: Number(categoryId) };
@@ -284,9 +285,10 @@ export default async function Category(props: Props) {
   });
 
   if (subcategories.length > 0) {
-    return <div className="@container text-2xl p-8">
-      Subcategory List Placeholder
-    </div>;
+    return <SubcategoryList
+      subcategories={subcategories}
+      title={await getTitle(props)}
+    />;
   }
 
   return (
