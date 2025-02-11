2dcb3be84bb000317e9b0e757449269fea5c7010
Chris Nanninga <chris.nanninga@bigcommerce.com>
2025-02-11 15:15:03 -0600

    Customize Soul card variables for subcategory list

--- core/components/subcategory-list/index.tsx
+++ core/components/subcategory-list/index.tsx
@@ -29,6 +29,11 @@ export const SubcategoryList = (
         <div className="mx-auto 
           grid grid-cols-1 gap-4 
           @lg:grid-cols-2 @2xl:grid-cols-3"
+
+          style={{
+            '--card-light-text': 'hsl(0 0% 0%)',
+            '--card-border-radius': '0',
+          } as CSSProperties}
         >
           {subcategories.map((subcategory) => (
             <Card
