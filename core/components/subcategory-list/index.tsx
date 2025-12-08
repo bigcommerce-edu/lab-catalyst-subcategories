03fc21762d1af0477097b2c1fd563a551e177877
Chris Nanninga <chris.nanninga@bigcommerce.com>
2025-12-08 14:39:13 -0600

    Add main subcategory list content

--- core/components/subcategory-list/index.tsx
+++ core/components/subcategory-list/index.tsx
@@ -13,10 +13,28 @@ export const SubcategoryList = (
     subcategories: Awaited<ReturnType<typeof getSubcategories>>,
   }
 ) => {
-  // TODO: Implement the main display of the category information
-  //  - Render an `h1` with the category title
-  //  - Loop through `subcategories` and render an empty element with the `entityId` as the key
   return (
-    <div></div>
+    <div className="@container 
+      mx-auto max-w-screen-2xl px-4 py-10 
+      @xl:px-6 @xl:py-14 @4xl:px-8 @4xl:py-12"
+    >
+      <h1 className="font-heading 
+        text-3xl font-medium leading-none 
+        @lg:text-4xl @2xl:text-5xl
+        text-contrast-400"
+      >
+          {title}
+      </h1>
+      <div className="w-full @container my-4">
+        <div className="mx-auto 
+          grid grid-cols-1 gap-4 
+          @lg:grid-cols-2 @2xl:grid-cols-3"
+        >
+          {subcategories.map((subcategory) => (
+            <div key={subcategory.entityId}></div>
+          ))}
+        </div>
+      </div>
+    </div>
   );
 };
