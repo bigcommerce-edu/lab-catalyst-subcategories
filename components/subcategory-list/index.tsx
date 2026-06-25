96594246215c1dfd5c45a94b993b4247f14d46e2
Chris Nanninga <tarsusc81@gmail.com>
2026-06-25 17:23:24 -0500

    TODO: Add main subcategory list content

--- components/subcategory-list/index.tsx
+++ components/subcategory-list/index.tsx
@@ -13,6 +13,9 @@ export const SubcategoryList = (
     subcategories: Awaited<ReturnType<typeof getSubcategories>>,
   }
 ) => {
+  // TODO: Implement the main display of the category information
+  //  - Render an `h1` with the category title
+  //  - Loop through `subcategories` and render an empty element with the `entityId` as the key
   return (
     <div></div>
   );
